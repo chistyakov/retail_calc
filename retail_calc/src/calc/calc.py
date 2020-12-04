fcd7156b7f87161c1e76d3ffec508ffb868af3e2
chistyakov <al.ol.chistyakov@gmail.com>
2020-12-04 07:15:42 +0300

    Discount: Naive implementation

--- retail_calc/src/calc/calc.py
+++ retail_calc/src/calc/calc.py
@@ -2,4 +2,22 @@ from decimal import Decimal
 
 
 def total_price_without_tax(quantity: int, unit_cost: Decimal) -> Decimal:
-    return quantity * unit_cost
+    total = quantity * unit_cost
+
+    discount_factor = 0
+    if total >= Decimal("1000"):
+        discount_factor = Decimal("0.03")
+
+    if total >= Decimal("5000"):
+        discount_factor = Decimal("0.05")
+
+    if total >= Decimal("7000"):
+        discount_factor = Decimal("0.07")
+
+    if total >= Decimal("10000"):
+        discount_factor = Decimal("0.1")
+
+    if total >= Decimal("50000"):
+        discount_factor = Decimal("0.15")
+
+    return total * (Decimal("1") - discount_factor)
--- retail_calc/tests/test_calc.py
+++ retail_calc/tests/test_calc.py
@@ -6,9 +6,33 @@ from calc.calc import total_price_without_tax
 
 class TestPriceWithoutTax(unittest.TestCase):
     def test_quantity_1(self):
-        price = total_price_without_tax(quantity=1, unit_cost=Decimal("1"))
-        self.assertEqual(price, Decimal("1"))
+        price = total_price_without_tax(quantity=1, unit_cost=Decimal("1.00"))
+        self.assertEqual(price, Decimal("1.00"))
 
     def test_quantity_2(self):
-        price = total_price_without_tax(quantity=2, unit_cost=Decimal("1"))
-        self.assertEqual(price, Decimal("2"))
+        price = total_price_without_tax(quantity=2, unit_cost=Decimal("1.00"))
+        self.assertEqual(price, Decimal("2.00"))
+
+    def test_quantity_999(self):
+        price = total_price_without_tax(quantity=999, unit_cost=Decimal("1.00"))
+        self.assertEqual(price, Decimal("999.00"))
+
+    def test_quantity_1000(self):
+        price = total_price_without_tax(quantity=1000, unit_cost=Decimal("1.00"))
+        self.assertEqual(price, Decimal("970.00"))
+
+    def test_quantity_4999(self):
+        price = total_price_without_tax(quantity=4999, unit_cost=Decimal("1.00"))
+        self.assertEqual(price, Decimal("4849.03"))
+
+    def test_quantity_5000(self):
+        price = total_price_without_tax(quantity=5000, unit_cost=Decimal("1.00"))
+        self.assertEqual(price, Decimal("4750.00"))
+
+    def test_quantity_49999(self):
+        price = total_price_without_tax(quantity=49999, unit_cost=Decimal("1.00"))
+        self.assertEqual(price, Decimal("44999.10"))
+
+    def test_quantity_50000(self):
+        price = total_price_without_tax(quantity=50000, unit_cost=Decimal("1.00"))
+        self.assertEqual(price, Decimal("42500.00"))
